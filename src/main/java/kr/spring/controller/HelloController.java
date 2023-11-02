f8d4e1824ac9b0dadc3a5bb3d2ddaaed9cb470bb
ojh <nbczerg98@naver.com>
2023-11-02 12:32:22 +0900

    commit

---
+++ src/main/java/kr/spring/controller/HelloController.java
@@ -0,0 +1,18 @@
+package kr.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	
+	@GetMapping("/hello")
+	public String hello() {
+		return "hello Spring Boot!";
+	}
+	
+}

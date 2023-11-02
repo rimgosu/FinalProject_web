fa6fecdeeb9bbeebc3717ffe3c4a9889759e6b43
ojh <nbczerg98@naver.com>
2023-11-02 16:08:14 +0900

    test3

--- src/main/java/kr/spring/controller/HelloController.java
+++ src/main/java/kr/spring/controller/HelloController.java
@@ -1,14 +1,10 @@
 package kr.spring.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-	
 	
 	@GetMapping("/hello")
 	public String hello() {

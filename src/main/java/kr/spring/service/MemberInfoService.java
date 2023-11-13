cae6c0575d0f94c2f5bc50c9d79875a0876d6f8d
juhyun <nbczerg98@naver.com>
2023-11-13 17:00:00 +0900

    juhyun

--- src/main/java/kr/spring/service/MemberInfoService.java
+++ src/main/java/kr/spring/service/MemberInfoService.java
@@ -5,15 +5,15 @@ import kr.spring.entity.MemberInfo;
 
 public interface MemberInfoService {
 
-	/* public void InsertMemberInfo(String username); */
+   /* public void InsertMemberInfo(String username); */
 
-	public void InsertMemberInfo(String nickname, String username, String password);
+   public void InsertMemberInfo(String nickname, String username, String password);
 
-	public int SelectMemberInfo(String username, String password);
+   public MemberInfo SelectMemberInfo(MemberInfo m);
 
-	public void InsertMemberInfoAdditional(MemberInfo info, String username_session);
+   public void InsertMemberInfoAdditional(MemberInfo info, String username_session);
 
-	
-	
-	
+   
+   
+   
 }
--- src/main/java/kr/spring/service/MemberInfoServiceImpl.java
+++ src/main/java/kr/spring/service/MemberInfoServiceImpl.java
@@ -4,6 +4,8 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartRequest;
+
 import com.datastax.oss.driver.api.core.CqlSession;
 import com.datastax.oss.driver.api.core.config.DriverConfigLoader;
 import com.datastax.oss.driver.api.core.cql.BoundStatement;
@@ -18,41 +20,44 @@ import kr.spring.entity.MemberInfo;
 @Service
 public class MemberInfoServiceImpl implements MemberInfoService{
 
-	@Override
-	public void InsertMemberInfo(String nickname, String username, String password) {
-		Path configPath = Paths.get("c:/keys/keyspace/application.conf");
+   @Override
+   public void InsertMemberInfo(String nickname, String username, String password) {
+      Path configPath = Paths.get("c:/keys/keyspace/application.conf");
         DriverConfigLoader loader = DriverConfigLoader.fromPath(configPath);
         
         try (CqlSession session = CqlSession.builder()
                 .withConfigLoader(loader)
                 .build()) {
-        	
-				String cql = """
-						insert into member.info ( 
-							nickname, username, password, register_date
-						) values (
-							?, ?, ?, toTimestamp(now())
-						)""";
-				PreparedStatement preparedStatement = session.prepare(cql);
-				session.execute(preparedStatement.bind( nickname, username, password));
-			
-	} }
+           
+            String cql = """
+                  insert into member.info ( 
+                     nickname, username, password, register_date
+                  ) values (
+                     ?, ?, ?, toTimestamp(now())
+                  )""";
+            PreparedStatement preparedStatement = session.prepare(cql);
+            session.execute(preparedStatement.bind( nickname, username, password));
+         
+   } }
 
-	@Override
-	public int SelectMemberInfo(String username, String password) {
-		Path configPath = Paths.get("c:/keys/keyspace/application.conf");
+   @Override
+   public MemberInfo SelectMemberInfo(MemberInfo m) {
+      Path configPath = Paths.get("c:/keys/keyspace/application.conf");
         DriverConfigLoader loader = DriverConfigLoader.fromPath(configPath);
+        String username = m.getUsername();
+        String password = m.getPassword();
+        
         
         try (CqlSession session = CqlSession.builder()
                 .withConfigLoader(loader)
                 .build()) {
-        	
-				String cql = "select username, password from member.info where username =? ";
-				PreparedStatement preparedStatement = session.prepare(cql);				
-				ResultSet resultSet = session.execute(preparedStatement.bind(username));
-				
-				String username_db = null;
-				String password_db= null;
+               
+            String cql = "select username, password from member.info where username =? ";
+            PreparedStatement preparedStatement = session.prepare(cql);            
+            ResultSet resultSet = session.execute(preparedStatement.bind(username));
+            
+            String username_db = null;
+            String password_db= null;
                 // Process the results
                 for (Row row : resultSet) {
                     // Access the columns in the result set using row.getXXX() methods
@@ -60,80 +65,86 @@ public class MemberInfoServiceImpl implements MemberInfoService{
                     password_db = row.getString("password"); }
                 
                 if (username_db.equals(username) && password_db.equals(password)) {
-                	System.out.println("로그인 완료");
-                	return 1;
+                   System.out.println("로그인 완료");
+                   return m;
                 }else {
-                	System.out.println("실패");
-                	System.out.println(username_db);
-                	System.out.println(password_db);
-                	return 0;
+                   System.out.println("실패");
+                   System.out.println(username_db);
+                   System.out.println(password_db);
+                   return m;
                 }
                     
                  
                 }
-	}
+   }
 
-	@Override
-	public void InsertMemberInfoAdditional(MemberInfo info, String username_session) {
-		// TODO Auto-generated method stub
-		Path configPath = Paths.get("c:/keys/keyspace/application.conf");
+   @Override
+   public void InsertMemberInfoAdditional(MemberInfo info, String username_session) {
+      // TODO Auto-generated method stub
+      Path configPath = Paths.get("c:/keys/keyspace/application.conf");
         DriverConfigLoader loader = DriverConfigLoader.fromPath(configPath);
+      /*
+       * //파일 업로드를 할 수 있게 도와주는 MultipartRequest 객체 생성. MultipartRequest multi = null;
+       * int fileMaxSize = 10*1024*1024; //파일 최대 크기 //저장경로 String savePath =
+       * request.getRealPath("resources/upload");
+       */
+        
         
         try (CqlSession session = CqlSession.builder()
                 .withConfigLoader(loader)
                 .build()) {
-        	
-				String cql = """
-						update member.info 
-						set age =?,phone=?,address=?,interest=?,mbti=?,
-						    sport=?,smoking=?,drinking=?,job=?,school=?,
-						    role=?,aboutme=? 
-						where username = ?;
-						""";
-				PreparedStatement preparedStatement = session.prepare(cql);
+           
+            String cql = """
+                  update member.info 
+                  set age =?,phone=?,address=?,interest=?,mbti=?,
+                      sport=?,smoking=?,drinking=?,job=?,school=?,
+                      role=?,aboutme=? 
+                  where username = ?;
+                  """;
+            PreparedStatement preparedStatement = session.prepare(cql);
 
-				session.execute(preparedStatement.bind( 
-						info.getAge(),
-				        info.getPhone(),
-				        info.getAddress(),
-				        info.getInterest(),
-				        info.getMbti(),
-				        info.getSport(),
-				        info.getSmoking(),
-				        info.getDrinking(),
-				        info.getJob(),
-				        info.getSchool(),
-				        info.getRole(),
-				        info.getAboutme(),
-				        username_session
-						));
-						
+            session.execute(preparedStatement.bind( 
+                  info.getAge(),
+                    info.getPhone(),
+                    info.getAddress(),
+                    info.getInterest(),
+                    info.getMbti(),
+                    info.getSport(),
+                    info.getSmoking(),
+                    info.getDrinking(),
+                    info.getJob(),
+                    info.getSchool(),
+                    info.getRole(),
+                    info.getAboutme(),
+                    username_session
+                  ));
+                  
 
-				System.out.println(info);
-			
-	} }
-	}
+            System.out.println(info);
+         
+   } }
+   }
 
-	
-	
+   
+   
 
-	
-	/*
-	 * @Override public void InsertMemberInfo(String username) { // TODO
-	 * Auto-generated method stub
-	 * 
-	 * Path configPath = Paths.get("c:/keys/keyspace/application.conf");
-	 * DriverConfigLoader loader = DriverConfigLoader.fromPath(configPath); //Use
-	 * DriverConfigLoader to load your configuration file try (CqlSession session =
-	 * CqlSession.builder() .withConfigLoader(loader) .build()) {
-	 * 
-	 * 
-	 * String cql = "insert into member.info (username) values (?)";
-	 * PreparedStatement preparedStatement = session.prepare(cql);
-	 * session.execute(preparedStatement.bind(username));
-	 * 
-	 * }
-	 * 
-	 * }
-	 */
-	
+   
+   /*
+    * @Override public void InsertMemberInfo(String username) { // TODO
+    * Auto-generated method stub
+    * 
+    * Path configPath = Paths.get("c:/keys/keyspace/application.conf");
+    * DriverConfigLoader loader = DriverConfigLoader.fromPath(configPath); //Use
+    * DriverConfigLoader to load your configuration file try (CqlSession session =
+    * CqlSession.builder() .withConfigLoader(loader) .build()) {
+    * 
+    * 
+    * String cql = "insert into member.info (username) values (?)";
+    * PreparedStatement preparedStatement = session.prepare(cql);
+    * session.execute(preparedStatement.bind(username));
+    * 
+    * }
+    * 
+    * }
+    */
+   
